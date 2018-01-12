928aa1c82061f525595d90a354ed805dd1feabad
Diana Ghinea <97.diana@gmail.com>
2018-01-12 22:27:03 +0200

    Added the User Profile model + migration

--- NotReddit/Readit/models.py
+++ NotReddit/Readit/models.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200)
+    #TODO (dianamin): Add user image.
+
+    def __str__(self):
+        return self.user.username
