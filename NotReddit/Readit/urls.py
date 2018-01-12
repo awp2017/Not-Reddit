e9be75f22f5a0ddd7cd94bf5c3d2eec19ebf0649
Diana Ghinea <97.diana@gmail.com>
2018-01-12 23:36:29 +0200

    added registration form

--- NotReddit/Readit/urls.py
+++ NotReddit/Readit/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.views import login
 
+from . import views
+
 urlpatterns = [
     url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^register/$', views.register, name='register'),
 ]
--- NotReddit/Readit/views.py
+++ NotReddit/Readit/views.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render(request, 'registration.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'registration.html', {'form': form})
