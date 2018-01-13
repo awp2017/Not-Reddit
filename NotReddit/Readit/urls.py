db4d83991b62c9b2969976fc95a9d5121865cc49
MadalinaAnghel <madalina.anghel948@yahoo.com>
2018-01-13 02:05:59 +0200

    Added PostDetail view

--- NotReddit/Readit/urls.py
+++ NotReddit/Readit/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
     url(r'^register/$', views.register, name='register'),
     url(r'^$', views.PostListView.as_view(), name='post_list'),
-
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetail.as_view(), name='post_detail'),
 ]
--- NotReddit/Readit/views.py
+++ NotReddit/Readit/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404 
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView 
 from Readit.models import Post
 
 # Create your views here.
@@ -15,6 +15,14 @@ class PostListView(ListView):
 
     def get_queryset(self, *args, **kwargs):
         return Post.objects
+
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = 'post_detail'
+    template_name = 'postDetail.html'
+
+    def get_object(self):
+        return get_object_or_404(Post, pk=self.kwargs['pk'])
 
 def register(request):
     if request.method =='POST':
